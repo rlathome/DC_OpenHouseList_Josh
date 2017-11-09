87c352f5d11664485c93c52e11e0632004c404a8
Josh Foster <josh@allenb.com>
2017-11-08 21:32:02 -0600

    misc

--- routes/index.js
+++ routes/index.js
@@ -91,7 +91,7 @@ router.get('/listing/:mls',function(req,res,next){
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     // console.log('body:', body); // Print the HTML for the Google homepage.
-    console.log('listing: ',JSON.parse(body))
+    // console.log('listing: ',JSON.parse(body))
     body=JSON.parse(body);
     res.json(body);
   });
